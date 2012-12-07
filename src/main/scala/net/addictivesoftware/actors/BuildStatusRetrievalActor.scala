dc64d0f0fb6768455a1b7fcee973025803ca831b
Gertjan Assies <gertjan.assies@gmail.com>
2012-12-07 11:27:20 +0100

    getting jenkins/hudson url from default.props now

--- src/main/scala/net/addictivesoftware/actors/BuildStatusRetrievalActor.scala
+++ src/main/scala/net/addictivesoftware/actors/BuildStatusRetrievalActor.scala
@@ -1,15 +1,14 @@
 package net.addictivesoftware.actors
 
 import net.liftweb.actor.LiftActor
-import net.liftweb.util.Schedule
+import net.liftweb.util.{Props, Schedule, HttpHelpers}
 import net.liftweb.util.Helpers._
-import net.liftweb.util.HttpHelpers
 import net.liftweb.json._
 import net.addictivesoftware.model.BuildStatus
 import dispatch._
 import java.util.Date
 import net.liftweb.mapper.By
-import net.liftweb.common.Full
+import net.liftweb.common.{Box, Full}
 
 object BuildStatusRetrievalActor extends LiftActor {
   case class RetrieveStatus(jobName:String, interval:Int)
@@ -19,63 +18,71 @@ object BuildStatusRetrievalActor extends LiftActor {
 
   implicit val formats = net.liftweb.json.DefaultFormats
 
+  private val jenkinsUrl:Box[String] = Props.get("jenkins.url")
+
   def messageHandler = {
     case RetrieveStatus(jobName, interval) =>
       if (!stopped)
         Schedule.schedule(this, RetrieveStatus(jobName, interval), interval minutes)
       try {
         println("Running job " + jobName)
-        val request = url("http://masterbuild01.ams.dev:8080/hudson/job/" + myUrlEncode(jobName) + "/lastBuild/api/json")
-        //val request = url("https://major7.ci.cloudbees.com/job/Medicate_GitHub/lastBuild/api/json") as("gertjan.assies@gmail.com", "4268f1a246970e114d4fb62e62c37b46")
-        val response:String = Http(request as_str)
+        jenkinsUrl match {
+          case Full(u) => {
+            val request = url(u.format(myUrlEncode(jobName)))
+            val response:String = Http(request as_str)
 
-        val bs = parse(response)
+            val bs = parse(response)
 
-        val isBuilding = (bs \ "building").extract[Boolean]
+            val isBuilding = (bs \ "building").extract[Boolean]
 
-        if (!isBuilding) {
+            if (!isBuilding) {
 
-          val date = new Date((bs \ "timestamp").extract[Long])
+              val date = new Date((bs \ "timestamp").extract[Long])
 
-          var culprits = ""
-          val culpritsList:List[String] = List()
-          val culpritsListJson = (bs \ "culprits" \ "fullName")
+              var culprits = ""
+              val culpritsList:List[String] = List()
+              val culpritsListJson = (bs \ "culprits" \ "fullName")
 
-          if (culpritsListJson.isInstanceOf[JArray])
-            culpritsList :: culpritsListJson.extract[List[String]]
-            if (culpritsList.size > 0) {
-              culprits = culpritsList.reduceLeft(_ + ", " + _)
-            }
-          else if (culpritsListJson.isInstanceOf[JString])
-            culprits = culpritsListJson.extract[String]
-          else
-            culprits = "..."
+              if (culpritsListJson.isInstanceOf[JArray])
+                culpritsList :: culpritsListJson.extract[List[String]]
+              if (culpritsList.size > 0) {
+                culprits = culpritsList.reduceLeft(_ + ", " + _)
+              }
+              else if (culpritsListJson.isInstanceOf[JString])
+                culprits = culpritsListJson.extract[String]
+              else
+                culprits = "..."
 
-          val buildStatus = new BuildStatus()
-            .job(jobName)
-            .buildId((bs \ "id").extract[String])
-            .number((bs \ "number").extract[Long])
-            .result((bs \ "result").extract[String])
-            .timestamp(date)
-            .culprits(culprits)
+              val buildStatus = new BuildStatus()
+                .job(jobName)
+                .buildId((bs \ "id").extract[String])
+                .number((bs \ "number").extract[Long])
+                .result((bs \ "result").extract[String])
+                .timestamp(date)
+                .culprits(culprits)
 
-          //only save a build status once
-          BuildStatus.find(
-                        By(BuildStatus.buildId, buildStatus.buildId),
-                        By(BuildStatus.job, jobName)) match {
-            case Full(bs) => {
-              println("already known build")
-            }
-            case (_) => {
-              val savedBS = buildStatus.saveMe();
-              println("saving " + savedBS)
+              //only save a build status once
+              BuildStatus.find(
+                By(BuildStatus.buildId, buildStatus.buildId),
+                By(BuildStatus.job, jobName)) match {
+                case Full(bs) => {
+                  println("already known build")
+                }
+                case (_) => {
+                  val savedBS = buildStatus.saveMe();
+                  println("saving " + savedBS)
+                }
+              }
+            } else {
+              println("skipping not finished build")
             }
           }
-      } else {
-        println("skipping not finished build")
-      }
+          case (_) => {
+            println("Url in properties file set?")
+          }
+        }
     } catch {
-      case e:Exception => {println(e.getMessage)}
+      case e:Exception => {println(e)}
     }
 
     case Stop =>
